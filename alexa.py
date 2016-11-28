51bbd0038b8e57049d30487b44a57aa09bdc87b1
Yash Ketkar <yashketkar@hotmail.com>
2016-11-27 22:40:53 -0500

    update alexa.py

--- alexa.py
+++ alexa.py
@@ -1,15 +1,33 @@
 import urllib
 import re
-print "Please enter the value of N:"
-n = int(raw_input())
-print "Getting top " + str(n) + " links from Alexa"
+import sys
+intError = "Please enter an integer between 1 to 500 only."
+ioError = "An IOError occured, Please make sure you are connected to the internet and python has access to it."
+def errorAndExit(error):
+    print error
+    sys.exit()
+try:
+    if len(sys.argv) == 2:
+        n = int(sys.argv[1])
+    else:
+        print "Please enter the value of N (1-500):"
+        n = int(raw_input())
+    if n>500 or n<1:
+        errorAndExit(intError)
+except ValueError:
+    errorAndExit(intError)
+print "Getting top " + str(n) + " link(s) from Alexa"
 print "Please wait..."
 alexatop500 = []
 for i in range(20):
-    alexatop500.extend(re.findall("<a href=\"/siteinfo/[^>]*>(.*?)</a>",urllib.urlopen("http://www.alexa.com/topsites/global;" + str(i)).read(),flags=0))
+    try:
+        alexatop500.extend(re.findall("<a href=\"/siteinfo/[^>]*>(.*?)</a>",\
+        urllib.urlopen("http://www.alexa.com/topsites/global;" + str(i)).read(),flags=0))
+    except IOError:
+        errorAndExit(ioError)
     if len(alexatop500)>=n:
         break
 print "Download complete."
-print "\nThe top " + str(n) + " links from Alexa are:"
+print "\nThe top " + str(n) + " link(s) from Alexa are:"
 for i in range(0,n):
     print i+1,alexatop500[i]
